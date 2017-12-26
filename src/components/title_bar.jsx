3d2c6450de8080fc57a3874165aa07db86821857
Simon Støvring <mail@simonbs.dk>
2017-12-26 15:52:55 +0100

    Adds badge

--- src/components/title_bar.jsx
+++ src/components/title_bar.jsx
@@ -4,29 +4,33 @@ import { withStyles } from 'material-ui/styles'
 import AppBar from 'material-ui/AppBar'
 import Typography from 'material-ui/Typography'
 import Toolbar from 'material-ui/Toolbar'
+import Badge from 'material-ui/Badge'
 import IconButton from 'material-ui/IconButton'
 import MenuIcon from 'material-ui-icons/Menu'
 
 const styles = {
   menuButton: {
-    marginLeft: '-22px'
+    marginLeft: -22
+  },
+  title: {
+    marginLeft: 8
   }
 }
 
 class TitleBar extends React.Component {
   render() {
-    const { classes } = this.props
     return (
       <AppBar position="static" elevation={0}>
         <Toolbar>
           <IconButton 
           onClick={this.props.onMenuClick} 
-          className={classes.menuButton} 
-          color="contrast"
-          className={this.props.classes.menuButton}>
-            <MenuIcon/>
+          className={this.props.classes.menuButton} 
+          color="contrast">
+            <Badge badgeContent={10} color="accent">
+              <MenuIcon/>
+            </Badge>
           </IconButton>
-          <Typography type="title" color="inherit" className={classes.flex}>
+          <Typography type="title" color="inherit" className={this.props.classes.title}>
             {this.props.title}
           </Typography>
         </Toolbar>
